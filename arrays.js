435d481a03d4dd856e4a19017337a9583890e6a5
Rafael Ruiz Morales <raf@Rafaels-MacBook-Pro.local>
2021-11-13 11:22:12 +0000

    Revising Basic Array functions adding more examples.

--- arrays.js
+++ arrays.js
@@ -78,13 +78,14 @@ const emojiMap = emojis.map((the_element, the_index, the_array) => {
 //// Reduce ////////////////////////////////////////////
 const arrayToReduce = [1, 2, 3, 4]
 const reducerFunction = (previousValue, currentValue) => {
-    console.log(`${previousValue} previousValue / ${currentValue} currentValue`)
+    // console.log(`${previousValue} previousValue / ${currentValue} currentValue`)
     return previousValue + currentValue
 }
 const accumulator = 0
 
 const reducedResult = arrayToReduce.reduce(reducerFunction, accumulator); // expected 10
-console.log(reducedResult)
+// console.log(reducedResult)
+
 // console.log([1, 2, 3, 4].reduce((previousValue, currentValue) => previousValue + currentValue));
 // --> previousValue = 0 / currentValue = 1
 //     0 previousValue + 1 currentValue = 1 <- new previousValue
@@ -97,24 +98,18 @@ console.log(reducedResult)
 //// Spread operator ... ////////////////////////////////////////////
 
     // 1- Merging Arrays
-    const array1 = [1, 2, 3];
-    const array2 = [4, 5, 6];
-
-    const mergedArray = [
-        ...array1,
-        ...array2
-    ]; // [ 1, 2, 3, 4, 5, 6 ]
+    const mergedArray = [ ...[1, 2, 3], ...[4, 5, 6] ]; // [ 1, 2, 3, 4, 5, 6 ]
 
     // 2- Clone Arrays
     const original = ['zero', 'one'];
-    const newArray = [...original];
+    const newArray = [...original]; // [ 'zero', 'one' ]
 
     // 3- String to Array
     const string = 'hi';
     const array = [...string]; // [ 'h' , 'i' ]
 
     // 4- Set to Array
-    const set = new Set([1, 2, 3]); // Set(3) {1, 2, 3}
+    const set = new Set([1, 2, 3, 1, 3]); // Set(3) {1, 2, 3}
     const arrayFromSet = [...set];  // array(3) [1, 2, 3]
 
     // 5- Arguments to Array
@@ -124,13 +119,20 @@ console.log(reducedResult)
         argumentsArray.map(name => console.log(`Hola ${name}`))
     }
 
-    // names('Jak', 'Sami'); 
+    // names('Jak', 'Sami', 'Yunlong'); 
     // 'Hola Jak'
     // 'Hola Sami'
+    // 'Hola Yunlong'
 
 
 // Destructuring ////////////////////////////////////////////
 const address = [221, 'Baker Street', 'London'];
-const [ houseNo, , city ] = address; // notice the white space between comas!
 
-// console.log(houseNo, city) // 221 'London'
+const [ houseNo1, street1 , city1 ] = address;
+// console.log(houseNo1, street1, city1) // 221 'Baker Street' 'London'
+
+const [ houseNo2, , city2 ] = address; // notice the white space between comas!
+// console.log(houseNo2, city2) // 221 'London'
+
+const [ , street3, ] = address; // notice the white space between comas!
+// console.log(street3) // Baker Street
